4cc3658942db713fee413dc8d9720096bbf9de4f
Prakash M <mprakash4678@gmail.com>
2026-01-21 10:25:16 +0530

    feat: dark, light mode

--- dom-tasks/count-characters/charactercount.js
+++ dom-tasks/count-characters/charactercount.js
@@ -70,3 +70,29 @@ document.getElementById("clearBtn").onclick = () => {
   textInput.value = "";
   resetStats();
 };
+
+
+const themeToggle = document.getElementById("themeToggle");
+const html = document.documentElement;
+
+if (localStorage.getItem("theme") === "dark") {
+  html.classList.add("dark");
+  themeToggle.innerHTML = `<i data-lucide="sun"></i>`;
+} else {
+  themeToggle.innerHTML = `<i data-lucide="moon"></i>`;
+}
+
+lucide.createIcons();
+
+themeToggle.addEventListener("click", () => {
+  html.classList.toggle("dark");
+
+  const isDark = html.classList.contains("dark");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+
+  themeToggle.innerHTML = isDark
+    ? `<i data-lucide="sun"></i>`
+    : `<i data-lucide="moon"></i>`;
+
+  lucide.createIcons();
+});
